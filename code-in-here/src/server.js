3c7c88cdcb6d0687c233c54e4b09fb5bd19991a0
Nyan <nyandude22@gmail.com>
2024-04-18 10:15:56 +0200

    fixed binding

--- code-in-here/src/server.js
+++ code-in-here/src/server.js
@@ -379,4 +379,4 @@ console.log('Existing user check complete:', existingUser);
 
 
 const PORT = process.env.PORT || 80;
-app.listen(PORT,'0,0,0,0', () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
